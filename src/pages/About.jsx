83309f537b5919605c1f1c2119c10c4d72d4252e
gru <gentlegru@protonmail.com>
2024-07-02 09:43:54 -0400

    portfolio

---
+++ src/pages/About.jsx
@@ -0,0 +1,58 @@
+import React from "react";
+import about_me_img from "../Image/about_me_img.png";
+import personal_info_data from "./assets/AboutIn";
+import "./About.css";
+function About() {
+  return (
+    <section className="about_me" id="about_myself">
+      <div className="container_01">
+        <div className="my_img">
+          <img id="img" src={about_me_img} alt="profile_image" />
+        </div>
+        <div className="my_info">
+          <h2>
+            About <span>Me</span>
+          </h2>
+          <p>
+            Experienced software engineers proficient in ASP.NET, Blockchain and
+            modern JS frameworks, I possess a strong background in full-stack
+            development, DevOps, and cloud technologies. Proficient in backend
+            development using a variety of languages to build high-performance
+            server-side applications and APIs. I am skilled in building scalable
+            applications on AWS using Docker and Kubernetes. I am currently
+            seeking a challenging role where I can leverage my expertise to
+            drive innovation and deliver high- quality software solutions.
+          </p>
+          <div className="personal_info">
+            {personal_info_data.map((data, index) => {
+              return (
+                <>
+                  <div className="abc" key={index}>
+                    {data.section_one.map((e, index) => {
+                      return (
+                        <h4 key={index}>
+                          <span>{e.data_type} :</span> {e.data_value}
+                        </h4>
+                      );
+                    })}
+                  </div>
+                  <div className="xyz">
+                    {data.section_two.map((e, index) => {
+                      return (
+                        <h4>
+                          <span>{e.data_type} :</span> {e.data_value}
+                        </h4>
+                      );
+                    })}
+                  </div>
+                </>
+              );
+            })}
+          </div>
+        </div>
+      </div>
+    </section>
+  );
+}
+
+export default About;
---
+++ src/pages/assets/ProjectsData.js
@@ -0,0 +1,37 @@
+import smart_inventory_img from '../../Image/proj_imgs/Brainhub.png';
+import consensys_codefi_img from '../../Image/proj_imgs/consensysCodefi.png';
+import memerial_keeper_img from '../../Image/proj_imgs/memerialKeeper.png';
+import interview_img from '../../Image/proj_imgs/interview.png';
+
+const proj_data = [
+  {
+    id: 1,
+    project_name: 'Smart Inventory',
+    proj_creation_date: 'Nov 2022',
+    project_image: smart_inventory_img,
+    proj_live_link: 'https://smartinventory.in/',
+  },
+  {
+    id: 2,
+    project_name: 'Consensys Codefi',
+    proj_creation_date: 'Apr 2020',
+    project_image: consensys_codefi_img,
+    proj_live_link: 'https://consensys.io/',
+  },
+  {
+    id: 3,
+    project_name: 'Memerial Keeper',
+    proj_creation_date: 'Oct 2021',
+    project_image: memerial_keeper_img,
+    proj_live_link: 'https://memorialkeeper.com/',
+  },
+  {
+    id: 4,
+    project_name: 'Interview Management System',
+    proj_creation_date: 'Apr 2017',
+    project_image: interview_img,
+    proj_live_link: '',
+  },
+];
+
+export default proj_data;
