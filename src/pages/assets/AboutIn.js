cfa875abfcbcc5995cc92cc2b1ad75d5f892c0fd
gru <gentlegru@protonmail.com>
2024-09-26 00:35:26 +0100

    darren

--- src/pages/assets/AboutIn.js
+++ src/pages/assets/AboutIn.js
@@ -2,44 +2,44 @@ const personal_info_data = [
   {
     index: 1,
     section_one: [
-      {
-        index: 1,
-        data_type: "Age",
-        data_value: "32",
-      },
-      {
-        index: 2,
-        data_type: "Gender",
-        data_value: "male",
-      },
-      {
-        index: 3,
-        data_type: "Languages",
-        data_value: "English",
-      },
+      // {
+      //   index: 1,
+      //   data_type: "Age",
+      //   data_value: "32",
+      // },
+      // {
+      //   index: 2,
+      //   data_type: "Gender",
+      //   data_value: "male",
+      // },
+      // {
+      //   index: 3,
+      //   data_type: "Languages",
+      //   data_value: "English",
+      // },
     ],
     section_two: [
-      {
-        index: 1,
-        data_type: "Phone",
-        data_value: "+48 22 389 6965",
-        icon_class: "fa-solid fa-phone-flip",
-        ic_color: "#e64a19",
-      },
-      {
-        index: 2,
-        data_type: "Email",
-        data_value: "bkuzeljevicfl@gmail.com",
-        icon_class: "fa-solid fa-envelope",
-        ic_color: "#00838F",
-      },
-      {
-        index: 3,
-        data_type: "Address",
-        data_value: "Warsaw Poland",
-        icon_class: "fa-sharp fa-solid fa-location-dot",
-        ic_color: "rgb(34 119 0)",
-      },
+      // {
+      //   index: 1,
+      //   data_type: "Phone",
+      //   data_value: "+48 22 389 6965",
+      //   icon_class: "fa-solid fa-phone-flip",
+      //   ic_color: "#e64a19",
+      // },
+      // {
+      //   index: 2,
+      //   data_type: "Email",
+      //   data_value: "bkuzeljevicfl@gmail.com",
+      //   icon_class: "fa-solid fa-envelope",
+      //   ic_color: "#00838F",
+      // },
+      // {
+      //   index: 3,
+      //   data_type: "Address",
+      //   data_value: "Warsaw Poland",
+      //   icon_class: "fa-sharp fa-solid fa-location-dot",
+      //   ic_color: "rgb(34 119 0)",
+      // },
     ],
   },
 ];
